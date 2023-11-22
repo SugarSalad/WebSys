c28598ac59343b0050c1aa2df9587958527d6eb3
Cyrus Tapalla <cyrusthetapalla25@gmail.com>
2023-11-22 16:39:22 +0800

    All tabs had now an Icon

--- WebSys/userForm.php
+++ WebSys/userForm.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="x-icon" href="./image/icon.png">
     <title>Billing Information</title>
     <link rel="stylesheet" href="./css/userForm.css"> <!-- Replace './css/styles.css' with your actual CSS file path -->
     <!-- Assuming you have a separate CSS file for styling -->
--- WebSys/waterBillForm.php
+++ WebSys/waterBillForm.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="x-icon" href="./image/icon.png">
     <title>Water Bill Form</title>
     <link rel="stylesheet" href="./css/billForm.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
