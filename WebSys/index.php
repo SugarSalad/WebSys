5d36dc65f7dbefa22412cff2204027c3e6873705
Cyrus Tapalla <cyrusthetapalla25@gmail.com>
2023-11-22 16:37:04 +0800

    Logo

--- WebSys/index.php
+++ WebSys/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login System</title>
+    <link rel="shortcut icon" type="x-icon" href="./image/icon.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="./css/login.css">
 </head>
