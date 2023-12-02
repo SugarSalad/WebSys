7600ec4330517e6ab876d126a2237f41c574a3fb
Cyrus Tapalla <95117575+SugarSalad@users.noreply.github.com>
2023-12-02 19:50:56 +0800

    User has OOP application

--- WebSys/php/displayuserbill.php
+++ WebSys/php/displayuserbill.php
@@ -2,34 +2,54 @@
 // Database Connection
 require "dbCon.php";
 
+class UserBillDisplay {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function displayUserBills($userID) {
+        $billData = array(); // Initialize as an empty array
+
+        // Query to call the SP_DisplayUserBill stored procedure
+        $sqlQuery = "CALL SP_DisplayUserBill('$userID')";
+
+        // Execute the stored procedure
+        $result = $this->conn->query($sqlQuery);
+
+        // Check if the query was successful
+        if ($result) {
+            // Fetch the result as an associative array
+            while ($row = $result->fetch_assoc()) {
+                // Format the amount with peso sign and commas
+                $row['Amount'] = '₱' . number_format($row['Amount'], 2);
+
+                // Add a comma for meter when it reaches 4 digits
+                $row['Meter'] = number_format($row['Meter'], 2);
+
+                $billData[] = $row;
+            }
+        } else {
+            // Handle the case where the query failed, e.g., log an error or show a message
+            echo "Database query failed: " . $this->conn->error;
+        }
+
+        return $billData;
+    }
+}
+
+// Create an instance of the UserBillDisplay class
+$userBillDisplay = new UserBillDisplay($conn);
+
 // Initialize variables
 $billData = array(); // Initialize as an empty array
 
 if (isset($_SESSION['UserID'])) {
     $userID = $_SESSION['UserID'];
 
-    // Query to call the SP_DisplayUserBill stored procedure
-    $sqlQuery = "CALL SP_DisplayUserBill('$userID')";
-
-    // Execute the stored procedure
-    $result = $conn->query($sqlQuery);
-
-    // Check if the query was successful
-    if ($result) {
-        // Fetch the result as an associative array
-        while ($row = $result->fetch_assoc()) {
-            // Format the amount with peso sign and commas
-            $row['Amount'] = '₱' . number_format($row['Amount'], 2);
-
-            // Add a comma for meter when it reaches 4 digits
-            $row['Meter'] = number_format($row['Meter'], 2);
-
-            $billData[] = $row;
-        }
-    } else {
-        // Handle the case where the query failed, e.g., log an error or show a message
-        echo "Database query failed: " . $conn->error;
-    }
+    // Display user bills using the UserBillDisplay class
+    $billData = $userBillDisplay->displayUserBills($userID);
 } else {
     // Redirect to the login page if the UserID is not set in the session
     header("Location: ../login.php");
