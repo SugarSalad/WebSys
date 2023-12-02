8a0d191ef5468225f3300351160ab1c51434616d
Kim Paolo R. Cuenca <Kim Paolo R. Cuenca@LAPTOP-2IA9591B>
2023-12-02 21:54:29 +0800

    update account oop

--- WebSys/php/updateAccount.php
+++ WebSys/php/updateAccount.php
@@ -5,55 +5,69 @@ session_start();
 // Database Connection
 require "dbCon.php";
 
-// Validation Method
-function validate($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+class AccountUpdater {
+    private $conn;
+
+    public function __construct($dbConnection) {
+        $this->conn = $dbConnection;
+    }
+
+    private function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    public function updateAccount($id, $username, $password, $level, $name, $housenum, $sex, $email) {
+        $id = $this->validate($id);
+        $username = $this->validate($username);
+        $password = $this->validate($password);
+        $level = $this->validate($level);
+        $name = $this->validate($name);
+        $housenum = $this->validate($housenum);
+        $sex = $this->validate($sex);
+        $email = $this->validate($email);
+
+        if (empty($id) || empty($username) || empty($password) || empty($level) || empty($name) || empty($housenum) || empty($sex) || empty($email)) {
+            return ['status' => 'error', 'message' => 'Missing values. Please fill in all fields.'];
+        } else {
+            $updateAccountQuery = "CALL SP_UpdateAccount($id, '$username', '$password', '$level', '$name', '$housenum', '$sex', '$email')";
+
+            $result = $this->conn->query($updateAccountQuery);
+
+            if ($result) {
+                return 'Account updated successfully.';
+            } else {
+                return 'Error: ' . $this->conn->error;
+            }
+        }
+    }
 }
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Validate user input
-    $id = validate($_POST['userID']);
-    $username = validate($_POST['uname']);
-    $password = validate($_POST['password']);
-    $level = validate($_POST['level']);
-    $name = validate($_POST['name']);
-    $housenum = validate($_POST['houseNumber']);
-    $sex = validate($_POST['gender']);
-    $email = validate($_POST['emailAdd']);
+    $accountUpdater = new AccountUpdater($conn);
 
-    // Check if data is empty
-    if (empty($id) || empty($username) || empty($password) || empty($level) || empty($name) || empty($housenum) || empty($sex) || empty($email)) {
-        // Send a JSON response indicating missing values
+    $result = $accountUpdater->updateAccount(
+        $_POST['userID'],
+        $_POST['uname'],
+        $_POST['password'],
+        $_POST['level'],
+        $_POST['name'],
+        $_POST['houseNumber'],
+        $_POST['gender'],
+        $_POST['emailAdd']
+    );
+
+    if (is_array($result)) {
         header('Content-Type: application/json');
-        echo json_encode(['status' => 'error', 'message' => 'Missing values. Please fill in all fields.']);
+        echo json_encode($result);
     } else {
-        // Call the stored procedure to update the account
-        $updateAccountQuery = "CALL SP_UpdateAccount($id, '$username', '$password', '$level', '$name', '$housenum', '$sex', '$email')";
-
-        
-        // Before the update query
-        echo "Before Update Query: UserID: $id, Username: $username, Password: $password, Level: $level, Name: $name, HouseNumber: $housenum, Sex: $sex, Email: $email";
-
-        $result = $conn->query($updateAccountQuery);
-
-        // After the update query
-        echo "After Update Query: Update successful. Rows affected: " . $conn->affected_rows;
-
-        // Check if the query was successful
-        if ($result) {
-            // Send a plain text response indicating success
-            header('Content-Type: text/plain');
-            echo 'Account updated successfully.';
-            exit();
-        } else {
-            // Send a plain text response indicating failure and include the error message
-            header('Content-Type: text/plain');
-            echo 'Error: ' . $conn->error;
-        }
+        header('Content-Type: text/plain');
+        echo $result;
     }
 }
+// Close the database connection
+$conn->close();
 ?>
